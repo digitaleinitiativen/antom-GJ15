88358699b596445652b886901aa8af5c8e0bbebb
Felix Salcher <felix.salcher@fusonic.net>
2015-09-26 13:27:23 +0200

    timers added

--- game.js
+++ game.js
@@ -6,8 +6,10 @@ var Antom = function(game) {
     this.dude = null;
     this.controls = null;
     this.stars = null;
-    this.score = 0;
-    this.scoreBox = null;
+    this.vitamins = 100;
+    this.vitaminText = null;
+    this.playerSpeed = 150;
+    this.speedText = null;
 };
 
 Antom.prototype = {
@@ -17,6 +19,7 @@ Antom.prototype = {
     },
     preload: function() {
         this.load.image('bg', 'assets/background.png');
+        this.load.image('star', 'assets/star.png');
         this.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     },
     create: function() {
@@ -25,6 +28,26 @@ Antom.prototype = {
 
         this.bg = this.add.tileSprite(0, 0, 3200, 2400, 'bg');
 
+        //-----Score Text----
+        var style = {
+            fill: "#fff"
+        };
+
+        this.vitaminText = this.add.text(10,10, "Vitamine: " + this.vitamins, style);
+        this.vitaminText.fixedToCamera = true;
+
+        this.speedText = this.add.text(10, 50, "PlayerSpeed: " + this.playerSpeed, style);
+        this.speedText.fixedToCamera = true;
+
+        //----Stars-----
+        this.stars = this.add.group();
+        this.stars.enableBody = true;
+        for(var i = 0; i < 40; i++) {
+            var star = this.stars.create(
+                Math.random() * this.world.width,
+                Math.random() * this.world.height,
+                'star');
+        }
 
         //----Initialize player----
         this.dude = this.add.sprite(game.world.centerX, game.world.centerY,'dude');
@@ -32,28 +55,47 @@ Antom.prototype = {
         this.dude.body.collideWorldBounds = true;
 
         this.camera.follow(this.dude);
+
+        this.time.events.loop(Phaser.Timer.SECOND, this.decreaseVitamins, this);
     },
     update: function() {
+
+        //-----Player movement--------
         this.dude.body.velocity.x = 0;
         this.dude.body.velocity.y = 0;
         this.dude.frame = 4;
 
         if(this.controls.left.isDown) {
-            this.dude.body.velocity.x = -150;
+            this.dude.body.velocity.x = -this.playerSpeed;
             this.dude.animations.play('left');
         }
         else if(this.controls.right.isDown) {
-            this.dude.body.velocity.x = 150;
+            this.dude.body.velocity.x = this.playerSpeed;
         }
 
         if(this.controls.up.isDown) {
-            this.dude.body.velocity.y = -150;
+            this.dude.body.velocity.y = -this.playerSpeed;
         }
         else if(this.controls.down.isDown) {
-            this.dude.body.velocity.y = 150;
+            this.dude.body.velocity.y = this.playerSpeed;
         }
+        //-------------------------------
 
+        //----Star collide----------
+        this.physics.arcade.overlap(this.dude,this.stars,function(dude,star) {
+            star.kill();
+            this.playerSpeed = this.playerSpeed + 50;
+            this.speedText.text = "PlayerSpeed: " + this.playerSpeed;
+        }, null, this);
+
+    },
+    decreaseVitamins: function() {
+        //---Decrease Vitamins---------
+        this.vitamins--;
+        this.vitaminText.text = "Vitamins: " + this.vitamins;
     }
 };
+
+
 
 game.state.add('LevelOne', Antom, true);
