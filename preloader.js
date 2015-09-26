bf04effc42767a457b0ce0e0579e9600601e892e
Christoph Klocker <christoph@vedanova.com>
2015-09-26 21:54:51 +0200

    added extension

--- preloader.js
+++ preloader.js
@@ -32,7 +32,7 @@ Antom.State.Preloader.prototype = {
     this.load.image('carrot', 'assets/carrot.png');
     this.load.image('ameisenbau', 'assets/001_Antom_House.png');
     this.load.image('antom', 'assets/antom.png');
-    this.load.spritesheet('001_Sprite_Arrow', 'assets/001_Sprite_Arrow',32,32);
+    this.load.spritesheet('001_Sprite_Arrow', 'assets/001_Sprite_Arrow.png',32,32);
     this.load.spritesheet('001_Enemy_Purple', 'assets/001_Enemy_Purple.png',32,32,2);
     this.load.spritesheet('001_Enemy_Blue', 'assets/001_Enemies_Sprite_Blue.png',32,32,2);
     this.load.spritesheet('dude', 'assets/001_Tom_Basic.png', 32, 32);
